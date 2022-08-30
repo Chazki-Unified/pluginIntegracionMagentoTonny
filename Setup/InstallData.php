ef50e47826405954ed8140d038fb7404d543fb62
daddychazki <111927268+daddychazki@users.noreply.github.com>
2022-08-30 11:54:00 -0500

    Update InstallData.php

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -36,7 +36,35 @@ class InstallData implements InstallDataInterface
      */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        $data = [];
+        $data = [
+
+            ['country_id' => 'PE', 'code' => 'PE-AMA', 'default_name' => 'Amazonas'],
+            ['country_id' => 'PE', 'code' => 'PE-ANC', 'default_name' => 'Ancash'],
+            ['country_id' => 'PE', 'code' => 'PE-APU', 'default_name' => 'Apurímac'],
+            ['country_id' => 'PE', 'code' => 'PE-ARE', 'default_name' => 'Arequipa'],
+            ['country_id' => 'PE', 'code' => 'PE-AYA', 'default_name' => 'Ayacucho'],
+            ['country_id' => 'PE', 'code' => 'PE-CAJ', 'default_name' => 'Cajamarca'],
+            ['country_id' => 'PE', 'code' => 'PE-CUS', 'default_name' => 'Cusco'],
+            ['country_id' => 'PE', 'code' => 'PE-CAL', 'default_name' => 'El Callao'],
+            ['country_id' => 'PE', 'code' => 'PE-HUV', 'default_name' => 'Huancavelica'],
+            ['country_id' => 'PE', 'code' => 'PE-HUC', 'default_name' => 'Huánuco'],
+            ['country_id' => 'PE', 'code' => 'PE-ICA', 'default_name' => 'Ica'],
+            ['country_id' => 'PE', 'code' => 'PE-JUN', 'default_name' => 'Junín'],
+            ['country_id' => 'PE', 'code' => 'PE-LAL', 'default_name' => 'La Libertad'],
+            ['country_id' => 'PE', 'code' => 'PE-LAM', 'default_name' => 'Lambayeque'],
+            ['country_id' => 'PE', 'code' => 'PE-LIM', 'default_name' => 'Lima'],
+            ['country_id' => 'PE', 'code' => 'PE-LOR', 'default_name' => 'Loreto'],
+            ['country_id' => 'PE', 'code' => 'PE-MDD', 'default_name' => 'Madre de Dios'],
+            ['country_id' => 'PE', 'code' => 'PE-MOQ', 'default_name' => 'Moquegua'],
+            ['country_id' => 'PE', 'code' => 'PE-LMA', 'default_name' => 'Municipalidad Metropolitana de Lima'],
+            ['country_id' => 'PE', 'code' => 'PE-PAS', 'default_name' => 'Pasco'],
+            ['country_id' => 'PE', 'code' => 'PE-PIU', 'default_name' => 'Piura'],
+            ['country_id' => 'PE', 'code' => 'PE-PUN', 'default_name' => 'Puno'],
+            ['country_id' => 'PE', 'code' => 'PE-SAM', 'default_name' => 'San Martín'],
+            ['country_id' => 'PE', 'code' => 'PE-TAC', 'default_name' => 'Tacna'],
+            ['country_id' => 'PE', 'code' => 'PE-TUM', 'default_name' => 'Tumbes'],
+            ['country_id' => 'PE', 'code' => 'PE-UCA', 'default_name' => 'Ucayali']
+        ];
 
         foreach ($data as $row) {
             $bind = [
